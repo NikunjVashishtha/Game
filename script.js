9b1277b535b872eeb07fb186cf4101f084b9e542
Nikunj Vashishtha <60650449+NikunjVashishtha@users.noreply.github.com>
2022-04-03 14:16:34 +0530

    Enhancements

--- script.js
+++ script.js
@@ -1,88 +1,209 @@
 window.onload = function() {
-    var timenow = Date.now();
-    let speed = 600;
-    let dir = 0;
+  // variables (speed & time)
+  var timenow = Date.now();
+  let speed = 600;
+  let dir = 0;
+  
+  // variables (dimensions)
+  const absol_h = window.innerHeight;
+  const absol_w = window.innerWidth;
 
-    const absol_h = window.innerHeight;
-    const absol_w = window.innerWidth;
+  // canavas data
+  var canvas = document.getElementById("game_canvas");
+  var ctx = canvas.getContext("2d");
+  ctx.canvas.width  = absol_w;
+  ctx.canvas.height = absol_h;
 
-    var canvas = document.getElementById("game_canvas");
-    var ctx = canvas.getContext("2d");
+  // image data
+  var img = new Image();
+  img.src = "rocket.png";
 
-    ctx.canvas.width  = absol_w;
-    ctx.canvas.height = absol_h;
+  // keys' variables & data
+  var x = 0;
+  var y = (absol_h-120)/2;
+  let up = document.getElementById("up");
+  let down = document.getElementById("down");
+  let left = document.getElementById("left");
+  let right = document.getElementById("right");
+  
+  // mouse_click_event
+  up.onmousedown = function() {dir = 4; setColor(dir);};
+  down.onmousedown = function() {dir = 3; setColor(dir);};
+  left.onmousedown = function() {dir = 2; setColor(dir);};
+  right.onmousedown = function() {dir = 1; setColor(dir);};
 
-    var img = new Image();
-    img.src = "rocket.png";
+  // touch_click_event
+  up.ontouchstart = function() {dir = 4; setColor(dir);};
+  down.ontouchstart = function() {dir = 3; setColor(dir);};
+  left.ontouchstart = function() {dir = 2; setColor(dir);};
+  right.ontouchstart = function() {dir = 1; setColor(dir);};
 
-    var x = 0;
-    var y = (absol_h-120)/2;
-    let up = document.getElementById("up");
-    let down = document.getElementById("down");
-    let left = document.getElementById("left");
-    let right = document.getElementById("right");
+  // mouse_release_event
+  up.onmouseup = function() {dir = 0; setColor(dir);};
+  down.onmouseup = function() {dir = 0; setColor(dir);};
+  left.onmouseup = function() {dir = 0; setColor(dir);};
+  right.onmouseup = function() {dir = 0; setColor(dir);};
 
-    up.onmousedown = function() {dir = 4};
-    down.onmousedown = function() {dir = 3};
-    left.onmousedown = function() {dir = 2};
-    right.onmousedown = function() {dir = 1};
+  //touch_release_event
+  up.ontouchend = function() {dir = 0; setColor(dir);};
+  down.ontouchend = function() {dir = 0; setColor(dir);};
+  left.ontouchend = function() {dir = 0; setColor(dir);};
+  right.ontouchend = function() {dir = 0; setColor(dir);};
+  
+  // keyboard_controls
+  $(document).ready(()=>{
+    // release_keys
 
-    up.ontouchstart = function() {dir = 4};
-    down.ontouchstart = function() {dir = 3};
-    left.ontouchstart = function() {dir = 2};
-    right.ontouchstart = function() {dir = 1};
+    $(document).bind('keyup', function(e) {
+      dir = 0;
+      setColor(dir);
+    });
 
-    up.onmouseup = function() {dir = 0};
-    down.onmouseup = function() {dir = 0};
-    left.onmouseup = function() {dir = 0};
-    right.onmouseup = function() {dir = 0};
+    //press WASD keys
+    $(document).bind('keypress', function(e) {
+      if(e.keyCode==115){
+        dir = 3;
+        setColor(dir);
+      }
+      else if(e.keyCode==97){
+        dir = 2;
+        setColor(dir);
+      }
+      else if(e.keyCode==119){
+        dir = 4;
+        setColor(dir);
+      }
+      else if(e.keyCode==100 || e.keyCode==39){
+        dir = 1;
+        setColor(dir);
+      }
+    });
 
-    up.ontouchend = function() {dir = 0};
-    down.ontouchend = function() {dir = 0};
-    left.ontouchend = function() {dir = 0};
-    right.ontouchend = function() {dir = 0};
+    // press arrow keys
+    $(document).bind('keydown', function(e) {
+      if(e.keyCode==40){
+        dir = 3;
+        setColor(dir);
+      }
+      else if(e.keyCode==97 || e.keyCode==37){
+        dir = 2;
+        setColor(dir);
+      }
+      else if(e.keyCode==119 || e.keyCode==38){
+        dir = 4;
+        setColor(dir);
+      }
+      else if(e.keyCode==100 || e.keyCode==39){
+        dir = 1;
+        setColor(dir);
+      }
+    });
+  })
 
-    function move()
+  // button color picker
+  function setColor(dir)
+  {
+    if (dir == 0)
     {
-      var ping = (Date.now()-timenow)/1000;
-      timenow = Date.now();
-      var fps = Math.round(1/ping);
-      ctx.clearRect(0,0,absol_w,absol_h);
-      ctx.beginPath();
-      ctx.font = '25px Sans-serif';
-      ctx.fillStyle = 'white';
-      ctx.fillText("FPS: "+fps,20,30);
-      ctx.beginPath();
-      ctx.drawImage(img,x,y,120,120);
-      if (dir == 1)
-      {
-        if (x+120 < absol_w)
-        {
-          x += (speed * ping);
-        }
-      }
-      else if (dir == 2)
-      {
-        if (x > 0)
-        {
-          x -= (speed * ping);
-        }
-      }
-      else if (dir == 3)
-      {
-        if (y+120 < absol_h)
-        {
-          y += (speed * ping);
-        }
-      }
-      else if (dir == 4)
-      {
-        if (y > 0)
-        {
-          y -= (speed * ping);
-        }
-      }
-      window.requestAnimationFrame(move);
+      up.style.backgroundColor = "white";
+      down.style.backgroundColor = "white";
+      left.style.backgroundColor = "white";
+      right.style.backgroundColor = "white";
     }
-    move();
+    else if (dir == 1)
+    {
+      right.style.backgroundColor = "rgb(245, 66, 96)";
+    }
+    else if (dir == 2)
+    {
+      left.style.backgroundColor = "rgb(182, 66, 245)";
+    }
+    else if (dir == 3)
+    {
+      down.style.backgroundColor = "rgb(66, 138, 245)";
+    }
+    else if (dir == 4)
+    {
+      up.style.backgroundColor = "rgb(37, 217, 91)";
+    }
+  }
+  
+  // movement of rocket
+  function move()
+  {
+    // variables
+    var ping = (Date.now()-timenow)/1000;
+    timenow = Date.now();
+    var fps = Math.round(1/ping);
+
+    // clear canvas
+    ctx.clearRect(0,0,absol_w,absol_h);
+
+    // fps
+    ctx.beginPath();
+    ctx.font = '25px Sans-serif';
+    ctx.fillStyle = 'white';
+    ctx.fillText("FPS: "+fps,20,30);
+
+    //rocket
+    ctx.beginPath();
+    ctx.drawImage(img,x,y,120,120);
+    
+    // motion
+
+    // right
+    if (dir == 1)
+    {
+      if (x+120 < absol_w)
+      {
+        x += (speed * ping);
+      }
+    }
+
+    // left
+    else if (dir == 2)
+    {
+      if (x > 0)
+      {
+        x -= (speed * ping);
+      }
+    }
+
+    // down
+    else if (dir == 3)
+    {
+      if (y+120 < absol_h)
+      {
+        y += (speed * ping);
+      }
+    }
+      
+    // up
+    else if (dir == 4)
+    {
+      if (y > 0)
+      {
+        y -= (speed * ping);
+      }
+    }
+    // animate
+    window.requestAnimationFrame(move);
+  }
+  // calling function
+  move();
+}
+document.getElementsByClassName("switch").item(1).addEventListener("click",controls_change_state());
+function controls_change_state()
+{
+  if (stats = true)
+  {
+    document.getElementById("controls_stats").innerText = "Controls: OFF";
+    document.getElementById("all_controls").style.display = "none";
+  }
+  else {
+    document.getElementById("controls_stats").innerText = "Controls: ON";
+    document.getElementById("all_controls").style.display = "block";
+  }
+  stats = !stats;
+  alert("no glitch");
 }
